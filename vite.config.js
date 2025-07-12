61ff0c1b62930200c82948a30c51c8410d1d0e94
Afzal14786 <mdafzal14777@gmail.com>
2025-07-12 19:39:55 +0530

    Modify vite.config.js file add server : {host : true, post: 5173}

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,            // Makes Vite listen on all addresses (0.0.0.0)
+    port: 5173,            // Optional: You can customize the port
+  },
 })
